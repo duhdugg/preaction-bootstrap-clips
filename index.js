356fac020710befd05a113d32bcbfefa0a1a1202
Doug Elkin <doug@drelyn.net>
2018-10-18 14:02:39 -0400

    reference default for imports

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const Alert = require('./dist/Alert.min.js')
-const Boilerplate = require('./dist/Boilerplate.min.js')
-const BuyButton = require('./dist/BuyButton.min.js')
-const Card = require('./dist/Card.min.js')
-const getClassesForColumn = require('./dist/getClassesForColumn.min.js')
-const Nav = require('./dist/Nav.min.js')
-const NavBar = require('./dist/NavBar.min.js')
-const NavItem = require('./dist/NavItem.min.js')
+const Alert = require('./dist/Alert.min.js').default
+const Boilerplate = require('./dist/Boilerplate.min.js').default
+const BuyButton = require('./dist/BuyButton.min.js').default
+const Card = require('./dist/Card.min.js').default
+const getClassesForColumn = require('./dist/getClassesForColumn.min.js').default
+const Nav = require('./dist/Nav.min.js').default
+const NavBar = require('./dist/NavBar.min.js').default
+const NavItem = require('./dist/NavItem.min.js').default
 
 module.exports = {
   Alert,
