623f4dd7d6c7ff8174ac95bf63ad68be6c2fdee2
Doug Elkin <doug@dougelkin.com>
2020-08-14 18:41:25 -0400

    first commit

---
+++ precache-manifest.9065b4d654435132712c9bc54ae8c7fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de1fb9d55d2d9afdd898",
+    "url": "build/2.de1fb9d5.js"
+  },
+  {
+    "revision": "0a3d8a862ad834975fcd3b8ef710a4b6",
+    "url": "build/2.de1fb9d5.js.LICENSE.txt"
+  },
+  {
+    "revision": "e46008162eaa4d8d14fe",
+    "url": "build/bundle.e4600816.js"
+  },
+  {
+    "revision": "836c2589d3d16f363108",
+    "url": "build/main.836c2589.js"
+  },
+  {
+    "revision": "38298c21e5f19a3d13b0a4001df95cf4",
+    "url": "index.html"
+  },
+  {
+    "revision": "836c2589d3d16f363108",
+    "url": "static/css/main.a3b2548c.chunk.css"
+  }
+]);
