3ba9febfa7ad2466e634edf70ce2a58abf85eb7a
Doug Elkin <doug@dougelkin.com>
2020-09-03 10:44:03 -0400

    rebuild docs

---
+++ precache-manifest.ef7776cd9199a658dac3b4616c8db7fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f246dfe23023cc921737",
+    "url": "build/2.f246dfe2.js"
+  },
+  {
+    "revision": "8e17500f72bd10872e55901acc28b924",
+    "url": "build/2.f246dfe2.js.LICENSE.txt"
+  },
+  {
+    "revision": "e46008162eaa4d8d14fe",
+    "url": "build/bundle.e4600816.js"
+  },
+  {
+    "revision": "d5e72083bd7e8d469718",
+    "url": "build/main.d5e72083.js"
+  },
+  {
+    "revision": "cffd6e5d78d2c93bc30e4dfa6a45935a",
+    "url": "index.html"
+  },
+  {
+    "revision": "d5e72083bd7e8d469718",
+    "url": "static/css/main.a4c90d64.chunk.css"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.f3621907b588ebf732a9534b32646e6f.js"
+  "precache-manifest.ef7776cd9199a658dac3b4616c8db7fc.js"
 );
 
 self.addEventListener('message', (event) => {
